2baa2a46a59e53f5357e9d53b305c64e308a299c
Anandhu S <anandhus642@gmail.com>
2025-12-08 20:45:39 +0400

    feat: Complete JavaScript Clean code lesson

---
+++ 6.Clean Code/script.js
@@ -0,0 +1,15 @@
+// ----------------Clean Code-----------------//
+
+//What is clean code?
+const x = function (z) {
+  const w = "Hello";
+
+  return w + z;
+};
+console.log(x(" Joie..."));
+
+const generateUserGreeting = function (name) {
+  const greeting = "Hello";
+  return greeting + name;
+};
+console.log(generateUserGreeting(" John"));
